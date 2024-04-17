ba7c53d681874f975d4b79fa678994531f6493b5
garg624 <gargayush970@gmail.com>
2024-04-17 13:56:12 +0530

    codemeet logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
-
+import logo from "@/public/codemeet_logo_png-removebg-preview.png"
 export default function Home() {
   return (
     <main className="">
       CodeMeet
+      <Image src={logo} alt="logo" className="w-60 h-40"/>
     </main>
   );
 }
